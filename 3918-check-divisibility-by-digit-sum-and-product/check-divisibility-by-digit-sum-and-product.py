de9d4fa34d1f057ffeac021002e8df86ba0cc48f
Gaurav Kumar <engcrigaurav@gmail.com>
2026-08-22 23:11:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (58.35%) - LeetSync

---
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        temp = n
+        digit_sum = 0
+        digit_product = 1
+
+        while temp > 0:
+            digit = temp % 10
+            digit_sum = digit_sum + digit
+            digit_product = digit_product * digit
+            temp //= 10
+
+        return n % (digit_sum + digit_product) == 0
+
+
+# Time Complexity = O(log n)
+# Space Complexity = O(1)
+        
