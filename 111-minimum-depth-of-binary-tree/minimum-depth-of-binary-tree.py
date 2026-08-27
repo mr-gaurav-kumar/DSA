95e0022ffc9f309f9d20002a48724b6f81551b73
Gaurav Kumar <engcrigaurav@gmail.com>
2026-08-27 23:48:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (69.56%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+class Solution:
+    def minDepth(self, root):
+        if not root:
+            return 0
+
+        queue = deque([(root, 1)])
+
+        while queue:
+            node, depth = queue.popleft()
+
+            # First leaf we encounter = minimum depth
+            if not node.left and not node.right:
+                return depth
+
+            if node.left:
+                queue.append((node.left, depth + 1))
+
+            if node.right:
+                queue.append((node.right, depth + 1))
+
+        return 0
+
+
+
+
+# Time Complexity = O(n)
+# Space Complexity = O(n)
